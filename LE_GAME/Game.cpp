0e26278de276532fd90cf21d4fbf58c22fa81fbb
ilyaan <ilyaan.ali@hotmail.com>
2024-11-18 16:14:22 +0100

    innitial commit

---
+++ LE_GAME/Game.cpp
@@ -0,0 +1,59 @@
+#include "pch.h"
+#include "Game.h"
+#include <iostream>
+
+//Basic game functions
+#pragma region gameFunctions											
+void Start()
+{
+}
+
+void Draw()
+{
+	ClearBackground();
+
+
+}
+
+void Update(float elapsedSec)
+{
+}
+
+void End()
+{
+	
+}
+#pragma endregion gameFunctions
+
+
+#pragma region inputHandling											
+void OnKeyDownEvent(SDL_Keycode key)
+{
+
+}
+
+void OnKeyUpEvent(SDL_Keycode key)
+{
+	
+}
+
+void OnMouseMotionEvent(const SDL_MouseMotionEvent& e)
+{
+	
+}
+
+void OnMouseDownEvent(const SDL_MouseButtonEvent& e)
+{
+	
+}
+
+void OnMouseUpEvent(const SDL_MouseButtonEvent& e)
+{
+	
+}
+#pragma endregion inputHandling
+
+#pragma region ownDefinitions
+// Define your own functions here
+
+#pragma endregion ownDefinitions
